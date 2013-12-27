79f3c56cf6a29d3931b0c12925172d4920a632af
RAY <ray@jawbone.com>
2013-12-26 16:10:33 -0800

    back to flask

--- websocketchat.py
+++ websocketchat.py
@@ -49,5 +49,5 @@ def chat():
 def hello():
     return 'Hello World!'
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', debug=True)
